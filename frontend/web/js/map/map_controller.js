28b4659fb609aeefb0fcf203cd7d18274c4822b7
johan <johankhorchi@gmail.com>
2016-03-18 22:41:57 +0100

    carte Maj

--- frontend/web/js/map/map_controller.js
+++ frontend/web/js/map/map_controller.js
@@ -1,11 +1,9 @@
 ulcodeIn24.controller('MapController', ['$scope', '$http', function($scope, $http){
 
-    var map = L.map('map', {
-        maxZoom: 17
-    });
+    var map = L.map('map').setView([48.6880756,6.1384176], 13);
 
-    L.tileLayer('http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=norges_grunnkart&zoom={z}&x={x}&y={y}', {
-        attribution: '&copy; <a href="http://kartverket.no/">Kartverket</a>'
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
     var photoLayer = L.photo.cluster({ spiderfyDistanceMultiplier: 1.2 }).on('click', function (evt) {
@@ -14,31 +12,20 @@ ulcodeIn24.controller('MapController', ['$scope', '$http', function($scope, $htt
             minWidth: 400
         }).openPopup();
     });
+    var photos = [];
 
-    reqwest({
-        url: 'http://kulturnett2.delving.org/api/search?query=*%3A*&format=jsonp&rows=100&pt=59.936%2C10.76&d=1&qf=abm_contentProvider_text%3ADigitaltMuseum',
-        type: 'jsonp',
-        success: function (data) {
-            var photos = [];
-            data = data.result.items;
+    // <img src="http://maps.googleapis.com/maps/api/streetview?size=600x300&location=48.692514,6.183341&heading=-13&fov=90&pitch=1&sensor=false" width="600" height="300" alt="Image Street View" />
 
-            for (var i = 0; i < data.length; i++) {
-                var photo = data[i].item.fields;
-                if (photo.abm_latLong) {
-                    var pos = photo.abm_latLong[0].split(',');
-                    photos.push({
-                        lat: pos[0],
-                        lng: pos[1],
-                        url: photo.delving_thumbnail[0],
-                        caption: (photo.delving_description ? photo.delving_description[0] : '') + ' - Kilde: <a href="' + photo.delving_landingPage + '">' + photo.delving_collection + '</a>',
-                        thumbnail: photo.delving_thumbnail[0]
-                    });
-                }
-            }
+    photos.push({
+        lat: 48.692514,
+        lng: 6.183341,
+        url: "http://maps.googleapis.com/maps/api/streetview?size=600x300&location=48.692514,6.183341&heading=-13&fov=90&pitch=1&sensor=false",
+        caption: "légende test",
+        thumbnail: "http://maps.googleapis.com/maps/api/streetview?size=600x300&location=48.692514,6.183341&heading=-13&fov=90&pitch=1&sensor=false"
 
-            photoLayer.add(photos).addTo(map);
-            map.fitBounds(photoLayer.getBounds());
-        }
     });
+
+    photoLayer.add(photos).addTo(map);
+    map.fitBounds(photoLayer.getBounds());
 
 }]);
