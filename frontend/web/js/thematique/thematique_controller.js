6161c801ba85d51384aec1faaec646bc2e4e63a3
johan <johankhorchi@gmail.com>
2016-03-19 02:47:59 +0100

    resolution bug

--- frontend/web/js/thematique/thematique_controller.js
+++ frontend/web/js/thematique/thematique_controller.js
@@ -1,8 +1,9 @@
 ulcodeIn24.controller('ThematiqueController', ['$scope', '$http', 'Thematique', function($scope, $http, Thematique) {
-$scope.commerces = 0;
 
     //get sur les series pour initialiser les variables dans serie.js
     var GetCommerce = function (ville) {
+        $scope.commerces = 0;
+
         $http.get('../api/commerces?ville=' + ville)
             .then(function (response) {
                        response.data.commerces.forEach(function (data) {
